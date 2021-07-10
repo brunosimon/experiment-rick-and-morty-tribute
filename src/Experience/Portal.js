55324df75fd11e20815732acdc78b6ec343df440
brunosimon <simon.bruno.77@gmail.com>
2021-07-10 18:22:31 +0200

    :sparkles: Portal > Add pull animation

--- src/Experience/Portal.js
+++ src/Experience/Portal.js
@@ -13,23 +13,37 @@ export default class Portal
         this.group = new THREE.Group()
         this.scene.add(this.group)
 
-        this.setMain()
+        this.setSurface()
         this.setLight()
     }
 
-    setMain()
+    setSurface()
     {
-        this.main = {}
+        this.surface = {}
+        this.surface.pull = {}
+        this.surface.pull.speed = 0
+        this.surface.pull.target = 0
+        this.surface.pull.value = 0
+
+        window.setInterval(() =>
+        {
+            this.surface.pull.target = (Math.random() - 0.5) * 0.2
+            window.setTimeout(() =>
+            {
+                this.surface.pull.target = 0
+            }, 100)
+        }, 3000)
 
         // Geometry
-        this.main.geometry = new THREE.PlaneGeometry(1, 1, 1, 1)
+        this.surface.geometry = new THREE.PlaneGeometry(1, 1, 15, 15)
 
         // Material
-        this.main.material = new THREE.ShaderMaterial({
+        this.surface.material = new THREE.ShaderMaterial({
             side: THREE.DoubleSide,
             uniforms:
             {
                 uTime: { value: 0 },
+                uPullStrength: { value: 0.1 },
                 uColor1: { value: new THREE.Color('#5cad4a') },
                 uColor2: { value: new THREE.Color('#208d45') },
                 uColor3: { value: new THREE.Color('#a7cb54') },
@@ -40,8 +54,8 @@ export default class Portal
         })
 
         // Mesh
-        this.main.mesh = new THREE.Mesh(this.main.geometry, this.main.material)
-        this.group.add(this.main.mesh)
+        this.surface.mesh = new THREE.Mesh(this.surface.geometry, this.surface.material)
+        this.group.add(this.surface.mesh)
     }
 
     setLight()
@@ -62,11 +76,20 @@ export default class Portal
 
     update()
     {
+        // Surface
+        const pullDelta = this.surface.pull.target - this.surface.pull.value
+        this.surface.pull.speed += pullDelta * 0.005 * this.time.delta
+        this.surface.pull.speed *= 0.97
+        this.surface.pull.value += this.surface.pull.speed
+
+        this.surface.material.uniforms.uPullStrength.value = this.surface.pull.value
+
+        // Light
         this.light.instance.position.copy(this.light.position)
         this.light.instance.intensity = 1 + Math.sin(this.time.elapsed * 0.0001 * 100) * 0.25
         this.light.instance.position.x = Math.sin(this.time.elapsed * 0.0001 * 100) * 0.02
         this.light.instance.position.y = Math.sin(this.time.elapsed * 0.00006 * 100) * 0.02
         this.light.instance.position.z = Math.sin(this.time.elapsed * 0.00004 * 100) * 0.02
-        this.main.material.uniforms.uTime.value = this.time.elapsed
+        this.surface.material.uniforms.uTime.value = this.time.elapsed
     }
 }
--- src/Experience/shaders/portalMain/vertex.glsl
+++ src/Experience/shaders/portalMain/vertex.glsl
@@ -1,8 +1,19 @@
+#define M_PI 3.1415926535897932384626433832795
+
+uniform float uPullStrength;
+
 varying vec2 vUv;
 
 void main()
 {
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    vec3 newPosition = position;
+    vec2 centeredUv = uv - 0.5;
+    float distanceToCenter = length(centeredUv) * 2.0;
+    float pullStrength = (cos(distanceToCenter * M_PI) + 1.0);
+    pullStrength *= uPullStrength;
+    newPosition.z += pullStrength;
+
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0);
 
     vUv = uv;
 }
